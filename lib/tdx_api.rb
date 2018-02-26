6272ebc269cb20d91f8ed1cd68d9949fa9104540
Jackson Potter <jackson.r.potter@gmail.com>
2018-02-25 23:13:17 -0500

    Fix for TdxApi::Snakeify not itializing

--- lib/tdx_api.rb
+++ lib/tdx_api.rb
@@ -1,5 +1,10 @@
-require "tdx_api/version"
+require 'dotenv/load'
+require 'rest-client'
+require 'jwt'
+require 'tdx_api/version'
+require 'tdx_api/named_struct'
 require 'tdx_api/client'
+require 'tdx_api/snakeify' # this must be required before ::Connection, since it depends on ::Snakeify
 require 'tdx_api/connection'
 require 'tdx_api/authentication'
 require 'tdx_api/ticket_app'
--- lib/tdx_api/connection.rb
+++ lib/tdx_api/connection.rb
@@ -1,5 +1,6 @@
 module TdxApi
   module Connection
+    include TdxApi::Snakeify
 
     def POST endpoint, payload
       response = RestClient.post(
@@ -7,8 +8,8 @@ module TdxApi
         payload,
         content_type: :json,
         accept: :json,
-        Authorization: "Bearer #{tdx_token}")
-      JSON.parse(response.body)
+        Authorization: "Bearer #{@bearer_token}")
+      snakeify(JSON.parse(response.body))
     end
 
     def GET endpoint
@@ -16,14 +17,38 @@ module TdxApi
         "https://api.teamdynamix.com/TDWebApi/api#{endpoint}",
         content_type: :json,
         accept: :json,
-        Authorization: "Bearer #{tdx_token}")
-      JSON.parse(response.body)
+        Authorization: "Bearer #{@bearer_token}")
+      snakeify(JSON.parse(response.body))
     end
 
     def DELETE
     end
 
     def PATCH
+    end
+
+    def underscore_key(key)
+      snake_case(key.to_s).to_sym
+    end
+
+    def snake_case(camel_cased_word)
+      camel_cased_word.to_s.gsub(/::/, '/')
+        .gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+        .gsub(/([a-z\d])([A-Z])/,'\1_\2')
+        .tr("-", "_")
+        .downcase
+    end
+
+    def snakeify(value)
+      case value
+        when Array
+          value.map { |v| snakeify(v) }
+          # or `value.map(&method(:snakeify))`
+        when Hash
+          Hash[value.map { |k, v| [underscore_key(k), snakeify(v)] }]
+        else
+          value
+       end
     end
 
   end
